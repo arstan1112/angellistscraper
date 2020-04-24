6f5f71bc08c3ac619ff9bf499545a1178bdbfe31
arstan1112 <arstan.omurzakov@gmail.com>
2020-04-24 19:56:47 +0600

    Refactoring, adding new parsing logic

--- src/Context/AngelContext.php
+++ src/Context/AngelContext.php
@@ -15,6 +15,7 @@ use Symfony\Component\HttpKernel\KernelInterface;
 
 class AngelContext implements Context
 {
+    const MAX_NUMBER_COMPANIES_PER_REQUEST = 400;
     /**
      * @var Session
      */
@@ -67,91 +68,195 @@ class AngelContext implements Context
         $this->loopLocations();
     }
 
-    private function loopLocations()
+    protected function loopLocations()
     {
-        $locations = ['Palo Alto', 'Moscow'];
-        $locationsInput = $this->session->getDriver()->find('//*[@id="location"]');
-        if ($locationsInput) {
-            foreach ($locations as $location) {
-                $this->session->getDriver()->wait(2000, "document.getElementsByClassName('more')");
-                $this->session->getDriver()->mouseOver('//*[@data-menu="locations"]');
-                $this->session->getDriver()->wait(2000, "document.getElementsByClassName('more')");
-
-                $this->session->getDriver()->setValue('//*[@id="location"]', $location);
-                $this->session->getDriver()->wait(5000, "document.getElementsByClassName('more')");
-
-                $dropDownMenu = $this->session->getDriver()->find('//*[@id="ui-id-3"]/li[1]');
+        $locations = ['Miami', 'Dallas'];
+        $locationsInputField = $this->session->getDriver()->find('//*[@id="location"]');
+        if ($locationsInputField) {
+            foreach ($locations as $locationName) {
+                $dropDownMenu = $this->setFilterName('locations', 'location', '3', $locationName);
                 if ($dropDownMenu) {
-                    $this->scrapeWithLocations($location);
+                    $this->scrapeWithLocations($locationName);
                 }
             }
         }
     }
 
-    private function scrapeWithLocations($location)
+    protected function loopTypes()
     {
-        $this->angelLogger->info('Drop down found');
+        $types = ['Startup', 'VC Firm', 'Private Company', 'SaaS', 'Incubator', 'Mobile App'];
+        foreach ($types as $type) {
+            $this->selectFilter('company_types', $type);
+            $totalCompanies = $this->countTotalCompanies();
+            if ($totalCompanies > self::MAX_NUMBER_COMPANIES_PER_REQUEST) {
+                $this->angelLogger->info('Type selected successfully');
+                $this->loopStages();
+            } elseif ($totalCompanies !== 0) {
+                $this->loopParseSave($type);
+            }
+            $this->removeFilter('2');
+        }
+    }
+
+    protected function loopStages()
+    {
+        $stages = ['Seed', 'Series A', 'Series B', 'Series C', 'Acquired'];
+        foreach ($stages as $stage) {
+            $this->selectFilter('stages', $stage);
+            $totalCompanies = $this->countTotalCompanies();
+            if ($totalCompanies > self::MAX_NUMBER_COMPANIES_PER_REQUEST) {
+                $this->angelLogger->info('Stage selected successfully');
+//                $this->loopMarkets();
+            } elseif ($totalCompanies !== 0) {
+                $this->loopParseSave($stage);
+            }
+        }
+    }
+
+    protected function loopMarkets()
+    {
+        $markets = ['Food and Beverages', 'Independent Pharmacies', 'Waste Management', 'Energy Efficiency', 'Small and Medium Businesses', 'Oil and Gas', 'Advertising Platforms', 'Virtualization', 'Restaurants', 'Hospitality', 'Heavy Industry', 'Families', 'Journalism', 'Chemicals', 'E-Commerce', 'Commodities', 'Material Science', 'Credit'];
+        $marketsInputField = $this->session->getDriver()->find('//*[@id="market"]');
+        if ($marketsInputField) {
+            foreach ($markets as $marketName) {
+                $dropDownMenu = $this->setFilterName('markets', 'market', '4', $marketName);
+                if ($dropDownMenu) {
+                    $this->scrapeWithMarkets($marketName);
+                }
+            }
+        }
+    }
+
+    protected function loopTechs()
+    {
+        $techs = ['Security', 'Cloud'];
+        $techsInputField = $this->session->getDriver()->find('//*[@id="tech"]');
+        if ($techsInputField) {
+            foreach ($techs as $techName) {
+                $dropDownMenu = $this->setFilterName('teches', 'tech', '5', $techName);
+                if ($dropDownMenu) {
+                    $this->scrapeWithTechs($techName);
+                }
+            }
+        }
+    }
+
+    protected function scrapeWithLocations($locationName)
+    {
+        $this->angelLogger->info('Drop down for locations found');
         $this->session->getDriver()->click('//*[@id="ui-id-3"]/li[1]');
         $this->session->getDriver()->wait(10000, "document.getElementsByClassName('more')");
-        $this->loopPages();
-        $this->getContent($location);
-        $this->angelLogger->info('File has been put to the directory');
-        $this->parseAndSave($location);
 
-        $totalCompanies = $this->session->getDriver()->find('//*[@class="count"]');
-        $totalCompaniesForThisLocation = preg_replace("/[^0-9]/", '', $totalCompanies[1]->getText());
-        $totalCompaniesForThisLocation = (int)$totalCompaniesForThisLocation;
+        $totalCompanies = $this->countTotalCompanies();
 
-        if ($totalCompaniesForThisLocation > 5) {
-            $this->loopMarkets();
-        } else {
-            $this->session->getDriver()->wait(6000, "document.getElementsByClassName('more')");
-            $this->session->getDriver()->click('//*[@class="clear_all_link hidden"]');
-            $this->session->getDriver()->wait(4000, "document.getElementsByClassName('more')");
+        if ($totalCompanies > self::MAX_NUMBER_COMPANIES_PER_REQUEST) {
+            $this->loopTypes();
+        } elseif ($totalCompanies !== 0) {
+            $this->loopParseSave($locationName);
         }
+        $this->removeAllFilters();
     }
 
-    private function loopMarkets()
+    protected function scrapeWithMarkets($market)
     {
-        $markets = ['Food and Beverages', 'Independent Pharmacies'];
-        $marketsInput = $this->session->getDriver()->find('//*[@id="market"]');
-        if ($marketsInput) {
-            foreach ($markets as $market) {
-                $this->session->getDriver()->wait(2000, "document.getElementsByClassName('more')");
-                $this->session->getDriver()->mouseOver('//*[@data-menu="markets"]');
-                $this->session->getDriver()->wait(2000, "document.getElementsByClassName('more')");
-
-                $this->session->getDriver()->setValue('//*[@id="market"]', $market);
-                $this->session->getDriver()->wait(5000, "document.getElementsByClassName('more')");
-
-                $dropDownMenu = $this->session->getDriver()->find('//*[@id="ui-id-4"]/li[1]');
-                if ($dropDownMenu) {
-                    $this->scrapeWithMarkets($market);
-                }
-            }
-        }
-    }
-
-    private function scrapeWithMarkets($market)
-    {
-        $this->angelLogger->info('Drop down found');
+        $this->angelLogger->info('Drop down for markets found');
         $this->session->getDriver()->click('//*[@id="ui-id-4"]/li[1]');
         $this->session->getDriver()->wait(10000, "document.getElementsByClassName('more')");
-        $this->loopPages();
-        $this->getContent($market);
-        $this->angelLogger->info('File has been put to the directory');
-        $this->parseAndSave($market);
 
+        $totalCompanies = $this->countTotalCompanies();
+
+        if ($totalCompanies > self::MAX_NUMBER_COMPANIES_PER_REQUEST) {
+            $this->loopTechs();
+        } elseif ($totalCompanies !== 0) {
+            $this->loopParseSave($market);
+        }
+        $this->removeFilter('3');
+    }
+
+    protected function scrapeWithTechs($tech)
+    {
+        $this->angelLogger->info('Drop down for techs found');
+        $this->session->getDriver()->click('//*[@id="ui-id-5"]/li[1]');
+        $this->session->getDriver()->wait(10000, "document.getElementsByClassName('more')");
+
+        $totalCompanies = $this->countTotalCompanies();
+
+        if ($totalCompanies > self::MAX_NUMBER_COMPANIES_PER_REQUEST) {
+            $this->angelLogger->info('Tech filter has more than 400 companies');
+        } elseif ($totalCompanies !== 0) {
+            $this->loopParseSave($tech);
+        }
+        $this->removeFilter('4');
+    }
+
+    protected function selectFilter(string $dataAttrMenu, string $dataAttrValue)
+    {
+        $this->session->getDriver()->wait(2000, "document.getElementsByClassName('more')");
+        $this->session->getDriver()->mouseOver('//*[@data-menu="' . $dataAttrMenu . '"]');
+        $this->session->getDriver()->wait(2000, "document.getElementsByClassName('more')");
+
+        $this->session->getDriver()->click('//*[@data-value="' . $dataAttrValue . '"]');
+        $this->session->getDriver()->wait(5000, "document.getElementsByClassName('more')");
+    }
+
+    protected function removeAllFilters()
+    {
         $this->session->getDriver()->wait(6000, "document.getElementsByClassName('more')");
-        $this->session->getDriver()->click('//*[@id="root"]/div[4]/div[2]/div/div[2]/div[1]/div[1]/div/div[2]/div[2]/img');
+        $this->session->getDriver()->click('//*[@class="clear_all_link hidden"]');
+        $this->session->getDriver()->wait(4000, "document.getElementsByClassName('more')");
+    }
 
+    protected function removeFilter(string $nodeNumber)
+    {
+        $this->session->getDriver()->wait(6000, "document.getElementsByClassName('more')");
+        $this->session->getDriver()->click('//*[@id="root"]/div[4]/div[2]/div/div[2]/div[1]/div[1]/div/div[2]/div[' . $nodeNumber . ']/img');
+        $this->session->getDriver()->wait(4000, "document.getElementsByClassName('more')");
+    }
+
+    protected function setFilterName(string $dataAttr, string $id, string $nodeNumber, string $filterName)
+    {
+        $this->session->getDriver()->wait(2000, "document.getElementsByClassName('more')");
+        $this->session->getDriver()->mouseOver('//*[@data-menu="' . $dataAttr . '"]');
+        $this->session->getDriver()->wait(2000, "document.getElementsByClassName('more')");
+
+        $this->session->getDriver()->setValue('//*[@id="' . $id . '"]', $filterName);
+        $this->session->getDriver()->wait(5000, "document.getElementsByClassName('more')");
+
+        $dropDownMenu = $this->session->getDriver()->find('//*[@id="ui-id-' . $nodeNumber . '"]/li[1]');
+
+        if ($dropDownMenu) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    protected function getContainer()
+    {
+        return $this->kernel->getContainer();
+    }
+
+    protected function countTotalCompanies()
+    {
+        $total = $this->session->getDriver()->find('//*[@class="count"]');
+        $totalCompanies = preg_replace("/[^0-9]/", '', $total[1]->getText());
+        $totalCompanies = (int)$totalCompanies;
+        return $totalCompanies;
+    }
+
+    protected function loopParseSave($nameSuffix)
+    {
+        $this->loopPages();
+        $this->putContentToFile($nameSuffix);
+        $this->angelLogger->info('Content has been put to the file');
+        $this->parseAndSave($nameSuffix);
     }
 
     protected function loopPages()
     {
         for ($page = 1; $page < 3; $page++) {
-            $mySearch = $this->session->getDriver()->find('//*[@class="more"]');
-            if ($mySearch) {
+            $moreButton = $this->session->getDriver()->find('//*[@class="more"]');
+            if ($moreButton) {
                 $this->session->getDriver()->click('//*[@class="more"]');
                 $this->session->getDriver()->wait(10000, "document.getElementsByClassName('more')");
             } else {
@@ -160,7 +265,7 @@ class AngelContext implements Context
         }
     }
 
-    protected function getContent($nameSuffix)
+    protected function putContentToFile($nameSuffix)
     {
         $date = new \DateTime();
         $nameSuffix = strtolower($nameSuffix);
@@ -172,18 +277,14 @@ class AngelContext implements Context
         file_put_contents($path . '/' . $name, '<html>' . $this->session->getPage()->getContent() . '</html>');
     }
 
-    protected function getContainer()
-    {
-        return $this->kernel->getContainer();
-    }
-
-    protected function parseAndSave($location)
+    protected function parseAndSave($nameSuffix)
     {
         $this->angelLogger->info('Parsing and saving has been initiated');
+
         $path = $this->getContainer()->getParameter('save_path');
-        $location = strtolower($location);
-        $location = preg_replace("/[^a-z\-]/", '', $location);
-        $name = 'angellist_' . $location . '.html';
+        $nameSuffix = strtolower($nameSuffix);
+        $nameSuffix = preg_replace("/[^a-z\-]/", '', $nameSuffix);
+        $name = 'angellist_' . $nameSuffix . '.html';
 
         $htmlFile = $path . '/' . $name;
         $html = file_get_contents($htmlFile);
@@ -212,6 +313,7 @@ class AngelContext implements Context
                 exit();
             }
         }
+        $this->angelLogger->info('Saving has been finished');
     }
 
     protected function parse(object $xpath, array $arr, int $counter)
